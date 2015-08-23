15b65c3169154973e70767827a5eb20f66c1341e
namakohime <hiroyuki.yamamoto.3123@gmail.com>
2015-08-23 19:37:18 +0900

    Update convert.c

--- convert.c
+++ convert.c
@@ -2,22 +2,21 @@
 
 int main( )
 {
-	FILE *fp;
-	int i, size;
-	unsigned char buf[1024];
-    char hex_string[1024];
-    
-	fp = fopen("test.bmp","rb");
-	if(fp != NULL) {
-		size = fread( &buf, sizeof(char), 128, fp);
-	}
-	fclose(fp);
+  FILE *fp;
+  int i, size;
+  unsigned char buf[1024];
+  char hex_string[1024];
+  
+  fp = fopen("test.bmp","rb");
+  if(fp != NULL) {
+    size = fread( &buf, sizeof(char), 128, fp);
+  }
+  fclose(fp);
 
-    for(i = 0; i < size*2; i++) {
-    	sprintf(&hex_string[3*i] , "%02X%s", buf[i], (i + 1) % 16 == 0 ? "¥n" : " ");
-    }
-    hex_string[size*2] = '¥0';
-    printf("%s ", hex_string);
-
-	return 0;
+  for(i = 0; i < size * 2; i++) {
+    sprintf(&hex_string[3 * i] , "%02X%s", buf[i], (i + 1) % 16 == 0 ? "¥n" : " ");
+  }
+  hex_string[size * 2] = '¥0';
+  printf("%s ", hex_string);
+  return 0;
 }
